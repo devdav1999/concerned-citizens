9b97b1f0ceff9c7fde95c747be5fe0d987cf1c2a
Alice <aliceyang013@gmail.com>
2020-06-11 13:26:39 -0700

    added input boxes to WriteArticle.js

--- client/src/components/pages/WriteArticle/WriteArticle.js
+++ client/src/components/pages/WriteArticle/WriteArticle.js
@@ -2,20 +2,45 @@ import React, { useState } from 'react';
 import './WriteArticle.css';
 
 function WriteArticle(props) {
-  const [title, setTitle] = useState('');
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [officialInfo, setOfficialInfo] = useState('');
+  const [reason, setReason] = useState('');
   const [content, setContent] = useState('');
 
-  function onChangeContent(ev) {
-    setContent(ev.target.value);
+  function inputContent(event) {
+    let value = setContent(event.target.value);
+    setContent(value);
   }
 
-  function onChangeTitle(ev) {
-    setTitle(ev.target.value);
+  function inputName(event) {
+    let value = setName(event.target.value);
+    console.log('inputName being clicked');
+    setName(value);
+  }
+
+  function inputAddress(event) {
+    let value = setAddress(event.target.value);
+    setAddress(value);
+  }
+
+  function inputOfficialInfo(event) {
+    let value = setOfficialInfo(event.target.value);
+    setOfficialInfo(value);
+  }
+
+  function inputReason(event) {
+    let value = setReason(event.target.value);
+    setReason(value);
   }
 
   function submit() {
+    console.log('submitted')
     const formData = {
-      title: title,
+      name: name,
+      address: address,
+      officialInfo: officialInfo,
+      reason: reason,
       text: content,
     };
     // Can also be written:
@@ -37,12 +62,36 @@ function WriteArticle(props) {
 
   return (
     <div className="WriteArticle">
-      <h1>Write an article</h1>
+      <h1>Write a letter as a concerned citizen</h1>
       <input
-          name="title"
-          placeholder="Title"
-          value={title}
-          onChange={onChangeTitle}
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={inputName}
+        />
+      <br />
+
+      <input
+          address="address"
+          placeholder="Address"
+          value={address}
+          onChange={inputAddress}
+        />
+      <br />
+
+      <input
+          officialInfo="officialInfo"
+          placeholder="Official's Name"
+          value={officialInfo}
+          onChange={inputOfficialInfo}
+        />
+      <br />
+
+      <input
+          reason="reason"
+          placeholder="Reason Why You Are Writing"
+          value={reason}
+          onChange={inputReason}
         />
       <br />
 
@@ -50,12 +99,29 @@ function WriteArticle(props) {
           name="content"
           placeholder="Contents"
           value={content}
-          onChange={onChangeContent}
+          onChange={inputContent}
         />
 
       <br />
 
-      <button onClick={submit}>Add to blog</button>
+      <button onClick={submit}>Submit</button>
+
+<br></br>
+
+      Address: {address}
+
+      <br></br>
+      <br></br>
+      Dear {officialInfo}, 
+      <br></br>
+      My name is {name} and I am writing to you because {reason}. {content}
+
+      <br></br>
+      <br></br>
+
+      Sincerely,
+      <br></br>
+      {name}
     </div>
   );
 }
