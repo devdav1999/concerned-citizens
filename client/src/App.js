a69d752188c28f99b5062045d24acd2ef921ed93
Alice <aliceyang013@gmail.com>
2020-06-17 14:50:00 -0700

    added functionality to submit component in share.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import { Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import LandingPage from './components/pages/LandingPage/LandingPage.js';
-import Blog from './components/pages/Blog/Blog.js';
+// import Blog from './components/pages/Blog/Blog.js';
+import Share from './components/pages/Share/Share.js';
 import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
+// import ComposeTemplate from './components/pages/ComposeTemplate/ComposeTemplate.js';
 
 function App () {
   return (
@@ -20,7 +22,7 @@ function App () {
       <div className="App-mainContent">
         <Switch>
           <Route exact path='/' component={LandingPage} />
-          <Route exact path='/blog/' component={Blog} />
+          <Route exact path='/share/' component={Share} />
           <Route exact path='/write/' component={WriteArticle} />
         </Switch>
       </div>
---
+++ client/src/components/pages/Share/Share.js
@@ -0,0 +1,184 @@
+import React, { useState, useEffect } from 'react';
+import GenerateLetter from './GenerateLetter';
+import InputInfo from './InputInfo';
+import './Share.css';
+import SubmitButton from './SubmitButton';
+// import Contents from './Contents';
+
+function Share() {
+  const [sharePosts, setSharePosts] = useState([]);
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [officialInfo, setOfficialInfo] = useState('');
+  const [reason, setReason] = useState('');
+  const [content, setContent] = useState('');
+
+  function inputName(event) {
+    setName(event.target.value);
+    console.log('setName being clicked');
+  }
+
+  function inputAddress(event) {
+    setAddress(event.target.value);
+  }
+
+  function inputOfficialInfo(event) {
+    setOfficialInfo(event.target.value);
+  }
+
+  function inputReason(event) {
+    setReason(event.target.value);
+  }
+
+  function inputContent(event) {
+    setContent(event.target.value);
+  }
+
+  function submit() {
+    console.log('submitted')
+    const formData = {
+      name: name,
+      address: address,
+      officialinfo: officialInfo,
+      reason: reason,
+      content: content,
+    };
+  }
+
+  function fetchPosts() {
+    console.log('Fetching data from API');
+    fetch('/api/mongodb/blogposts/')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got data back', data);
+        setSharePosts(data);
+      });
+  }
+
+  function deleteArticle(documentId) {
+    console.log('Sending DELETE for', documentId);
+    // Do the DELETE, using "?_id=" to specify which document we are deleting
+    fetch('/api/mongodb/blogposts/?_id=' + documentId, {
+        method: 'DELETE',
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got this back', data);
+
+        // Call method to refresh data
+        fetchPosts();
+      });
+  }
+
+  function voteArticle(article) {
+    let newVoteCount = article.voteCount;
+
+    // Increase the vote count 
+    if (!newVoteCount) {
+      newVoteCount = 1;
+    } else {
+      newVoteCount++;
+    }
+
+    const formData = {
+      voteCount: newVoteCount,
+    };
+
+    // Do the PUT, using "?_id=" to specify which document we are affecting
+    const documentId = article._id;
+    fetch('/api/mongodb/blogposts/?_id=' + documentId, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got this back', data);
+
+        // Call method to refresh data
+        fetchPosts();
+      });
+  }
+
+  // Invoke fetchPosts on initial load
+  useEffect(fetchPosts, []);
+
+  return (
+
+
+      <div className="Container">
+        <div className="WriteArticle">
+          <h1>My Information:</h1>
+
+            <InputInfo
+
+              InputName
+                name = {name}
+                inputName = {inputName}
+
+              InputAddress
+                address = {address}
+                inputAddress = {inputAddress}
+
+              InputOfficialInfo
+                officialinfo = {officialInfo}
+                inputOfficialInfo = {inputOfficialInfo}
+
+              InputReason
+                reason = {reason}
+                inputReason = {inputReason}
+
+              InputContent
+                inputContent ={content}
+                inputContent = {inputContent}
+
+            />
+
+          <div className="SubmitButton">
+            <SubmitButton 
+              submit = {submit}
+            />
+          </div>
+        </div>
+            
+      <div className="GenerateLetter">
+
+        {/* <GenerateLetter
+          name = {name}
+          address = {address}
+          officialinfo = {officialInfo}
+          reason = {reason}
+          content = {content}
+      /> */}
+      
+      <div className="">
+      <h1>Your Letter</h1>
+      {
+        sharePosts.map((post, index) => (
+          <div className="Share-article" key={post._id}>
+
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+
+            <div className="Share-articleActions">
+              <div onClick={() => deleteArticle(post._id)}>
+                <span alt="delete this">🗑</span>
+              </div>
+              <div onClick={() => voteArticle(post)}>
+                <span alt="upvote this">⬆ {post.voteCount}</span>
+              </div>
+            </div>
+          </div>
+        ))
+      }
+      </div>
+
+
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Share;
